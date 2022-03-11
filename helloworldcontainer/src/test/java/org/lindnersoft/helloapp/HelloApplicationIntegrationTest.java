d2b50ada911c2736d7cfcc9ef117c15c3ad93d2a
Scott Lindner <scott.lindner@parsons.com>
2022-03-11 12:28:12 -0700

    trying to setup building the test container on the fly... not working

--- helloworldcontainer/src/test/java/org/lindnersoft/helloapp/HelloApplicationIntegrationTest.java
+++ helloworldcontainer/src/test/java/org/lindnersoft/helloapp/HelloApplicationIntegrationTest.java
@@ -11,50 +11,48 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.File;
-import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testcontainers
 public class HelloApplicationIntegrationTest {
 
-    private static final int port = 8080;
-
-    // Test data
-    private URI targetUri;
-
+    private static final int CONTAINER_EXPOSED_PORT = 8080;
+    private static final String TEST_MESSAGE = "What a lovely integration test.";
 
     private static File dockerfileFile = new File("Dockerfile");
-    private static String dockerFilePath = dockerfileFile.getAbsolutePath();
+    private static String dockerfileFileAbsolutePath = dockerfileFile.getAbsolutePath();
 
     public static ImageFromDockerfile imageFromDockerfile = new ImageFromDockerfile()
-            .withFileFromFile("Dockerfile", dockerfileFile);
+            .withFileFromPath("Dockerfile", dockerfileFile.toPath());
 
     private static String dockerImageName = "helloworldcontainer:1.0-SNAPSHOT";
 
     @Container
     public static GenericContainer helloAppTestContainer
             = new GenericContainer(dockerImageName)
-            .withExposedPorts(port)
-            .withEnv("MESSAGE", "Hello from the integration test.");
+            .withExposedPorts(CONTAINER_EXPOSED_PORT)
+            .withEnv("MESSAGE", TEST_MESSAGE);
+
+//    @Container
+//    public static GenericContainer helloAppTestContainer2
+//            = new GenericContainer(imageFromDockerfile)
+//            .withExposedPorts(CONTAINER_EXPOSED_PORT)
+//            .withEnv("MESSAGE", TEST_MESSAGE);
 
     @BeforeEach
     public void beforeEach() {
     }
 
     @Test
-    public void remoteProtoRequest() {
+    public void testHelloAppContainer() {
         String containerIpAddress = helloAppTestContainer.getContainerIpAddress();
         Integer containerPort = helloAppTestContainer.getFirstMappedPort();
 
-        targetUri = URI.create(containerIpAddress + ":" + containerPort);
-
         RestTemplate restTemplate = new RestTemplate();
-        String resourceUrl = "http://" + targetUri.toString();
+        String resourceUrl = "http://" + containerIpAddress + ":" + containerPort;
         ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl, String.class);
         assertEquals(response.getStatusCode(), HttpStatus.OK);
-
-        assertTrue(true);
+        assertEquals(response.getBody(), TEST_MESSAGE);
     }
 }
